cda36a649b273a39999eab3b3c7591c17429ff23
Chandrakant-pradhan <pradhanchandrakant13@gmail.com>
2024-07-02 23:18:30 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,13 @@ const app = express()
 
 //need some middlewares express.json and cors
 app.use(express.json())
-app.use(cors())
+
+const corsOptions ={
+    origin:'http://localhost:8000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 //prepare the routes
 app.use("/api/user" , userRoute);
