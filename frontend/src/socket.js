559191712a99e18dc8d99ab4757adc5b990f4c3b
Chandrakant Pradhan <132835748+Chandrakant-pradhan@users.noreply.github.com>
2024-07-03 22:32:19 +0530

    Update socket.js
    socket connection with https://e2ee-messaging-service.onrender.com

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8000";
+const URL = "https://e2ee-messaging-service.onrender.com";
 const socket = io(URL, { autoConnect: false });
 
 
