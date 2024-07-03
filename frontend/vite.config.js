eaff8197cad366466f6812f9ca16b1272017abf6
Chandrakant Pradhan <132835748+Chandrakant-pradhan@users.noreply.github.com>
2024-07-03 21:07:29 +0530

    Update vite.config.js
    changing proxy to https://e2ee-messaging-service.onrender.com/

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://e2ee-messaging-service.onrender.com/',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
